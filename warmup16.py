7d0b7ed7f8c8182c17480ceef7cd0a5c695ebf97
jemro774 <31485871+jemro774@users.noreply.github.com>
2017-12-11 14:41:28 -0500

    Updated from Brython Server: 12/11/2017 2:41:27 PM

--- warmup16.py
+++ warmup16.py
@@ -1,1 +1,10 @@
+#Jack Robey
+#12/11/17
+#warmup16.py - finds all the words in the dictionary that start with your first initial and end with your last initial
 
+dictionary = open('engmix.txt')
+
+for word in dictionary:
+    if len(word) > 0:
+        if word.strip()[0] == 'j' and word.strip()[-1] == 'r':
+            print(word.strip())

d1fc388a6af2e6c1f7f1834e52adb9abf83d54ed
jemro774 <31485871+jemro774@users.noreply.github.com>
2017-12-13 13:28:26 -0500

    Updated from Brython Server: 12/13/2017 1:28:25 PM

--- quiz6.py
+++ quiz6.py
@@ -1,1 +1,8 @@
+#Jack Robey 
+#12/13/17 
+#quiz6.py 
 
+dictionary = open('engmix.txt') 
+for word in dictionary: 
+    if word.count('c') == 3 and word.count('p') == 2: 
+        print(word.strip())
